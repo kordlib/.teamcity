8b0e6b8ca80995ce1573828ae0070f0a348c546f
Michael Rittmeister <michael@rittmeister.in>
2024-01-29 00:03:24 +0100

    Add qodana

--- .teamcity/dev/kord/build/cache/Project.kt
+++ .teamcity/dev/kord/build/cache/Project.kt
@@ -5,11 +5,28 @@ import dev.kord.build.createProject
 import dev.kord.build.publishing.applyPublishingParameters
 import jetbrains.buildServer.configs.kotlin.RelativeId
 import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.buildSteps.qodana
 
 val Cache = createProject("cache", "feature/native") {
     project.params.applyPublishingParameters()
 
     project.id = RelativeId("cache")
+    addBuildType {
+        name = "Code Quality"
+        id = RelativeId("qodana")
+
+        requirements {
+            matches("teamcity.agent.os.family", "Linux")
+        }
+
+        steps {
+            qodana {
+                linter = jvm()
+                cloudToken = "credentialsJSON:3a7564f6-e577-4553-a387-80c2686bde05"
+                additionalQodanaArguments = "--baseline .ci/qodana.sarif.json"
+            }
+        }
+    }
     addBuildType {
         applyMultiplatformMatrix()
 
