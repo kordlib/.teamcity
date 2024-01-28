2cec5cfaeee7027035162417c57eaa45881e1153
Michael Rittmeister <michael@rittmeister.in>
2024-01-28 19:36:40 +0100

    Project cleanup

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,31 +1,6 @@
-import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
-import jetbrains.buildServer.configs.kotlin.projectFeatures.buildReportTab
+import jetbrains.buildServer.configs.kotlin.project
 import jetbrains.buildServer.configs.kotlin.projectFeatures.githubAppConnection
-import jetbrains.buildServer.configs.kotlin.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
-
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
+import jetbrains.buildServer.configs.kotlin.version
 
 version = "2023.11"
 
@@ -33,11 +8,6 @@ project {
     description = "Contains all other projects"
 
     features {
-        buildReportTab {
-            id = "PROJECT_EXT_1"
-            title = "Code Coverage"
-            startPage = "coverage.zip!index.html"
-        }
         githubAppConnection {
             id = "PROJECT_EXT_4"
             displayName = "Kord CI"
@@ -57,44 +27,5 @@ project {
             preventDependencyCleanup = false
         }
     }
-
-    subProject(Cache)
 }
 
-
-object Cache : Project({
-    name = "Cache"
-
-    vcsRoot(Cache_HttpsGithubComKordlibCacheRefsHeadsMaster)
-
-    buildType(Cache_Build)
-})
-
-object Cache_Build : BuildType({
-    name = "Build"
-
-    vcs {
-        root(Cache_HttpsGithubComKordlibCacheRefsHeadsMaster)
-    }
-
-    triggers {
-        vcs {
-        }
-    }
-
-    features {
-        perfmon {
-        }
-    }
-})
-
-object Cache_HttpsGithubComKordlibCacheRefsHeadsMaster : GitVcsRoot({
-    name = "https://github.com/kordlib/cache#refs/heads/master"
-    url = "https://github.com/kordlib/cache"
-    branch = "refs/heads/master"
-    branchSpec = "refs/heads/*"
-    authMethod = password {
-        userName = "DRSchlaubi"
-        password = "credentialsJSON:abb87df6-3a83-4342-b3fa-41c919da36a0"
-    }
-})
