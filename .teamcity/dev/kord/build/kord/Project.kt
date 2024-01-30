2180d909caba4b5ed3da97306aac42b9a74d4b59
Michael Rittmeister <michael@rittmeister.in>
2024-01-30 17:57:43 +0100

    Build curl if needed

--- .teamcity/dev/kord/build/kord/Project.kt
+++ .teamcity/dev/kord/build/kord/Project.kt
@@ -5,6 +5,8 @@ import dev.kord.build.publishing.applyPublishingParameters
 import dev.kord.build.steps.qodana
 import dev.kord.build.steps.runTestsAndPublishWithGradle
 import jetbrains.buildServer.configs.kotlin.RelativeId
+import jetbrains.buildServer.configs.kotlin.buildFeatures.buildCache
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
 private const val token = "tc_token_id:CID_ead29039499734d5f53ebb99e1e14bf5:-1:14179102-c8b1-4427-82dc-d7ab819ae5d7"
 
@@ -13,5 +15,32 @@ val Kord = createProject("kord", "main", token) {
 
     project.id = RelativeId("kord")
     qodana("credentialsJSON:bcc43ac3-b408-44d2-bb5a-361fe53b5652")
-    runTestsAndPublishWithGradle()
+    runTestsAndPublishWithGradle({
+        buildCache {
+            name = "curl_build"
+            rules = "curl_build"
+        }
+    }) {
+        script {
+            name = "Install curl"
+            conditions {
+                matches("teamcity.agent.os.family", "Linux")
+            }
+
+            workingDir = "curl_build"
+            scriptContent = """
+                if [ ! -e Makefile ]; then
+                    curl -o curl.tgz https://curl.se/download/curl-8.5.0.tar.gz
+                    tar xzvf curl.tgz --strip-components=1
+                    rm curl.tgz
+                    ./configure --with-openssl --enable-websockets
+                fi
+                if [ ! -e src/curl ]; then
+                    make
+                fi
+                make install
+                cp -r /usr/local/lib/libcurl* /usr/lib64/
+            """.trimIndent()
+        }
+    }
 }
--- .teamcity/dev/kord/build/steps/Gradle.kt
+++ .teamcity/dev/kord/build/steps/Gradle.kt
@@ -2,14 +2,22 @@ package dev.kord.build.steps
 
 import dev.kord.build.ProjectContext
 import dev.kord.build.applyMultiplatformMatrix
+import jetbrains.buildServer.configs.kotlin.BuildFeatures
+import jetbrains.buildServer.configs.kotlin.BuildSteps
 import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
 
-fun ProjectContext.runTestsAndPublishWithGradle() = addBuildType {
+fun ProjectContext.runTestsAndPublishWithGradle(
+    features: BuildFeatures.() -> Unit = {},
+    beforeSteps: BuildSteps.() -> Unit = {}
+) = addBuildType {
     applyMultiplatformMatrix()
 
     name = "Build & Push"
     id = childId("build")
+
+    features(features)
     steps {
+        beforeSteps()
         gradle {
             id = "checks"
             tasks = "testOnCurrentOS"
