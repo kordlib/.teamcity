6b40760306a057bd08721ed917eba881a4de2a37
Michael Rittmeister <michael@rittmeister.in>
2024-03-01 02:41:01 +0100

    Update kordx.emoji token

--- .teamcity/dev/kord/build/emoji/Project.kt
+++ .teamcity/dev/kord/build/emoji/Project.kt
@@ -5,7 +5,7 @@ import dev.kord.build.publishing.applyPublishingParameters
 import dev.kord.build.steps.runTestsAndPublishWithGradle
 import jetbrains.buildServer.configs.kotlin.RelativeId
 
-private const val token = "tc_token_id:CID_ead29039499734d5f53ebb99e1e14bf5:-1:7ca49a71-b885-4a77-a5b8-ccb6108471d1"
+private const val token = "tc_token_id:CID_ead29039499734d5f53ebb99e1e14bf5:-1:92aea938-40e1-421f-8926-79db8fa10b8d"
 
 val Emoji = createProject("emoji", "main", token, gitName = "kordx.emoji") {
     project.params.applyPublishingParameters()
