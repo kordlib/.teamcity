cd1c7363979ceb47ca8d90d5a3db26c188ec6a3e
Michael Rittmeister <michael@rittmeister.in>
2024-06-21 21:47:54 +0200

    Add missing parameters to Lavalink.kt

--- .teamcity/dev/kord/build/lavalink/Lavalink.kt
+++ .teamcity/dev/kord/build/lavalink/Lavalink.kt
@@ -1,12 +1,14 @@
 package dev.kord.build.lavalink
 
 import dev.kord.build.createProject
+import dev.kord.build.publishing.applyPublishingParameters
 import dev.kord.build.steps.runTestsAndPublishWithGradle
 import jetbrains.buildServer.configs.kotlin.RelativeId
 
 private const val token = "tc_token_id:CID_ead29039499734d5f53ebb99e1e14bf5:-1:0224f02c-a618-4782-8239-75364ab55f97"
 
 val Lavalink = createProject("Lavalink.kt", "main", token) {
+    project.params.applyPublishingParameters()
     project.id = RelativeId("LavalinkKt")
 
     runTestsAndPublishWithGradle(useSeparateHosts = false)
