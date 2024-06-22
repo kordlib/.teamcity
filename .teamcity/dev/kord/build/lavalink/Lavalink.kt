e8aa9767e77f608db878c6674766710f9f247d19
Michael Rittmeister <michael@rittmeister.in>
2024-06-22 15:08:03 +0200

    Update invalid credentials for Lavalink.kt

--- .teamcity/dev/kord/build/lavalink/Lavalink.kt
+++ .teamcity/dev/kord/build/lavalink/Lavalink.kt
@@ -8,7 +8,7 @@ import jetbrains.buildServer.configs.kotlin.RelativeId
 private const val token = "tc_token_id:CID_ead29039499734d5f53ebb99e1e14bf5:-1:0224f02c-a618-4782-8239-75364ab55f97"
 
 val Lavalink = createProject("Lavalink.kt", "main", token) {
-    project.params.applyPublishingParameters()
+    project.params.applyPublishingParameters(usePublisherPortal = true)
     project.id = RelativeId("LavalinkKt")
 
     runTestsAndPublishWithGradle(useSeparateHosts = false)
--- .teamcity/dev/kord/build/publishing/Publishing.kt
+++ .teamcity/dev/kord/build/publishing/Publishing.kt
@@ -3,9 +3,14 @@ package dev.kord.build.publishing
 import jetbrains.buildServer.configs.kotlin.ParameterDisplay
 import jetbrains.buildServer.configs.kotlin.ParametrizedWithType
 
-fun ParametrizedWithType.applyPublishingParameters() {
-    secretEnv("ORG_GRADLE_PROJECT_mavenCentralUsername", "credentialsJSON:732f0780-6752-42bc-a222-48c171a06b19")
-    secretEnv("ORG_GRADLE_PROJECT_mavenCentralPassword", "credentialsJSON:56a86492-6c2d-4ed4-8121-6fdec2cb1155")
+fun ParametrizedWithType.applyPublishingParameters(usePublisherPortal: Boolean = false) {
+    if (usePublisherPortal) {
+        secretEnv("ORG_GRADLE_PROJECT_mavenCentralUsername", "credentialsJSON:74b2fea6-b88d-4d1f-a357-b96aa4e6b3e2")
+        secretEnv("ORG_GRADLE_PROJECT_mavenCentralPassword", "credentialsJSON:773c3b77-79dd-43d5-bd82-60d09c88a6c5")
+    } else {
+        secretEnv("ORG_GRADLE_PROJECT_mavenCentralUsername", "credentialsJSON:732f0780-6752-42bc-a222-48c171a06b19")
+        secretEnv("ORG_GRADLE_PROJECT_mavenCentralPassword", "credentialsJSON:56a86492-6c2d-4ed4-8121-6fdec2cb1155")
+    }
     secretEnv("ORG_GRADLE_PROJECT_signingInMemoryKey", "credentialsJSON:ddba5b0d-4fc1-4e83-b94f-748c53b0f1e0")
     secretEnv(
         "ORG_GRADLE_PROJECT_signingInMemoryKeyPassword",
