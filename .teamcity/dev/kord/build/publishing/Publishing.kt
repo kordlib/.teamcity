0c8e219dfd7872dbcc4c500b8e8dbbb0e326f662
Michael Rittmeister <michael@rittmeister.in>
2024-06-22 15:20:28 +0200

    Update non publisher portal creds

--- .teamcity/dev/kord/build/publishing/Publishing.kt
+++ .teamcity/dev/kord/build/publishing/Publishing.kt
@@ -8,8 +8,8 @@ fun ParametrizedWithType.applyPublishingParameters(usePublisherPortal: Boolean =
         secretEnv("ORG_GRADLE_PROJECT_mavenCentralUsername", "credentialsJSON:74b2fea6-b88d-4d1f-a357-b96aa4e6b3e2")
         secretEnv("ORG_GRADLE_PROJECT_mavenCentralPassword", "credentialsJSON:773c3b77-79dd-43d5-bd82-60d09c88a6c5")
     } else {
-        secretEnv("ORG_GRADLE_PROJECT_mavenCentralUsername", "credentialsJSON:732f0780-6752-42bc-a222-48c171a06b19")
-        secretEnv("ORG_GRADLE_PROJECT_mavenCentralPassword", "credentialsJSON:56a86492-6c2d-4ed4-8121-6fdec2cb1155")
+        secretEnv("ORG_GRADLE_PROJECT_mavenCentralUsername", "credentialsJSON:ea81b569-741e-409f-bed9-00fc98319845")
+        secretEnv("ORG_GRADLE_PROJECT_mavenCentralPassword", "credentialsJSON:0deeae31-c62a-446d-83d8-d978d6f69ad5")
     }
     secretEnv("ORG_GRADLE_PROJECT_signingInMemoryKey", "credentialsJSON:ddba5b0d-4fc1-4e83-b94f-748c53b0f1e0")
     secretEnv(
