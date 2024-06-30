1ab219f0cd7e578c31f1254ec2a7166a49588f8d
Michael Rittmeister <michael@rittmeister.in>
2024-06-30 15:30:16 +0200

    Add token for CodeGen.kt

--- .teamcity/dev/kord/build/codegen/Project.kt
+++ .teamcity/dev/kord/build/codegen/Project.kt
@@ -6,7 +6,7 @@ import jetbrains.buildServer.configs.kotlin.RelativeId
 import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
-private val token = ""
+private val token = "tc_token_id:CID_ead29039499734d5f53ebb99e1e14bf5:-1:ba37a7ce-9c76-4146-9dc7-f6d1dd01ed6b"
 
 val CodegenKt = createProject("CodeGen.kt", "main", token, "codegen-kt") {
     project.params.applyPublishingParameters()
