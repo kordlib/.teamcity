f7c71a25fda8c3cf99a811e88b3dd8a08c54b049
Michael Rittmeister <michael@rittmeister.in>
2025-09-19 02:43:49 +0200

    Update version

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -8,7 +8,7 @@ import jetbrains.buildServer.configs.kotlin.project
 import jetbrains.buildServer.configs.kotlin.projectFeatures.githubAppConnection
 import jetbrains.buildServer.configs.kotlin.version
 
-version = "2025.03"
+version = "2025.07"
 
 project {
     description = "Contains all other projects"
